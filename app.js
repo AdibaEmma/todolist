6470a1bcf9dd7ba0e2403189ebdea1b32ff3e439
Emmanuel Adiba <eabaagah@gmail.com>
2020-11-12 23:47:49 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items = [];
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    let today = new Date();
+    
+    let day = today.toLocaleDateString("en-US", options);
+
+    
+    res.render("list", {
+        kindOfDay: day,
+        newListItems: items
+    });
+    
+    app.post("/", (req, res) => {
+       let item = req.body.newItem;
+
+        items.push(item);
+
+
+        res.redirect("/")
+    })
+
+});
+
+
+
+
+
+const port = process.env.port || 3000;
+
+app.listen(port, function () {
+    console.log(`server listening on port ${port}`)
+});
