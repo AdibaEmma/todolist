c9e76c19108c38de8d37009377928eb7b9ff3264
Emmanuel Adiba <eabaagah@gmail.com>
2020-11-15 00:42:28 +0000

    layout restructure

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const body = require("body-parser");
 
 const app = express();
 
 let items = ["Sweep Room", "Eat Breakfast", "Take Lesson"];
+let workItems = [];
 
-app.use(bodyParser.urlencoded({
+app.use(body.urlencoded({
     extended: false
 }));
 
@@ -27,22 +28,43 @@ app.get("/", function (req, res) {
 
 
     res.render("list", {
-        kindOfDay: day,
+        listTitle: day,
         newListItems: items
     });
 
-    app.post("/", (req, res) => {
-        let item = req.body.newItem;
-
-        items.push(item);
-
-
-        res.redirect("/")
-    })
 
 });
 
+app.post("/", (req, res) => {
+    let item = req.body.newItem;
 
+    if (req.body.list === "Work") {
+        workItems.push(item);
+
+        res.redirect("/work")
+    } else {
+        items.push(item);
+
+        res.redirect("/");
+    }
+
+});
+
+app.get("/work", (req, res) => {
+
+    res.render("list", {
+        listTitle: "Work List",
+        newListItems: workItems
+    });
+});
+
+app.post("/work", (req, res) => {
+
+    let item = req.body.newItem;
+    workItems.push(item);
+
+    res.redirect("/work");
+});
 
 
 
